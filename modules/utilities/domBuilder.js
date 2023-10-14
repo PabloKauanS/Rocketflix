38a34f2ed70f794936bc98e57d589678da0af09a
pablo kauan <pablokauanoficial@gmail.com>
2023-10-14 20:10:48 -0300

    responsive-add

--- modules/utilities/domBuilder.js
+++ modules/utilities/domBuilder.js
@@ -4,18 +4,15 @@ export default async function domBuilder() {
   const data_movie = await requestApi();
   const image_movie = data_movie["poster_path"]
     ? `https://image.tmdb.org/t/p/original${data_movie["poster_path"]}`
-    : "../icons/favico/poster.png";
+    : "./icons/favico/poster.png";
   const title_movie = data_movie["title"]
     ? data_movie["title"]
     : "Ops, hoje não é dia de assistir filme. Bora codar! 🚀";
-  const data_description = data_movie["overview"]
-    ? data_movie["overview"]
-    : "";
+  const data_description = data_movie["overview"] ? data_movie["overview"] : "";
   const str_movie = `<img class="movie-image" src="${image_movie}" width="171" height="243.773" alt="">
         <div class="movie-info">
           <h2 class="font-4 movie-title">${title_movie}</h2>
           <p class="font-5 movie-description">${data_description}</p>
         </div>`;
-  console.log(data_movie);
   dom_movie.innerHTML = str_movie;
 }
